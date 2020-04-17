59af3fb94c362ac57a31dae9e5db990b684fedf2
Rogério Munhoz <roz@rjmunhoz.me>
2020-04-17 13:01:29 -0300

    :tada: Inital commit

---
+++ README.md
@@ -0,0 +1,178 @@
+Boas vindas ao primeiro projeto de NodeJS!
+---
+
+Você já usa o GitHub diariamente para desenvolver os exercícios, certo? Agora, para desenvolver os projetos, você deverá seguir as instruções a seguir. Fique atento a cada passo, e se tiver qualquer dúvida, nos envie por Slack! #vqv 🚀
+
+Aqui você vai encontrar os detalhes de como estruturar o desenvolvimento do seu projeto a partir deste repositório, utilizando uma branch específica e um Pull Request para colocar seus códigos.
+
+## O que deverá ser desenvolvido
+
+Você vai refatorar parte de um CLI (command line interface) para que, ao invés de utilizar callbacks, utilize apenas Promises. Além disso, você vai implementar mais algumas funcionalidades, consumindo a mesma API que está sendo consumida. Você também adicionará testes para toda a funcionalidade desenvolvida por você.
+
+## Desenvolvimento
+
+O CLI mostra informações sobre o mundo de Game of Thrones, utilizando, como fonte dessas informações, uma API pública chamada [An API of Ice And Fire](anapioficeandfire.com).
+
+O código deste repositório possui a funcionalidade de listar as personagens, e exibir os detalhes sobre uma personagem selecionada. Além de refatorar o código já existente, você deverá adcioinar a funcionalidade de buscar livros pelo nome, exibir os resultados, e mostrar os detalhes do livro selecionado.
+
+Este repositório possui testes para toda a funcionalidade que já está desenvolvida. Ao refatorar o código, garanta que todos esses testes estão passando, para que você tenha certeza de que a funcionalidade continua a mesma.
+
+## Requisitos do projeto
+
+### 1 - A cobertura de testes do projeto deverá ser maior ou igual a 90%
+
+### 2 - O projeto deve ser feito, necessariamente, utilizando Promises
+
+Não pode haver nenhum tipo de código síncrono, ou que utilize callbacks.
+
+Você pode trocar a biblioteca utilizada para fazer requisições HTTP, se preferir, mas a biblioteca atual já suporta Promises.
+
+Você também pode utilizar async/await sempre que precisar manipular as promises, não sendo necessário consumi-las utilizando `then` e `catch`.
+
+### 3 - Exibir, no menu inicial, o sub-menu "livros" e, dentro dele, uma opção "Pesquisar livros"
+
+Ao selecionar essa opção, permitir que o usuário insira o nome do livro que deseja pesquisar.
+
+### 4 - Utilizando o nome inserido, realizar uma requisição para o endpoint `/books` da API, com o parâmetro `?name` contendo o nome digitado pelo usuário.
+
+Você pode consultar [a documentação](https://www.anapioficeandfire.com/Documentation#books) deste endpoint para verificar qual o formato em que os dados serão retornados.
+
+### 5 - Apresentar os resultados para o usuário numa lista
+
+A lista deve exibir apenas o nome do livro, e permitir que o usuário escolha um dos livros para o qual deseja ver os detalhes.
+
+### 6 - Apresentar as opções "Próxima página" e "Página anterior" caso existam mais de 10 resultados
+
+Ambas as opções só devem ser exibidas quando de fato forem úteis, ou seja, se o usuário já estiver na primeira página, a opção "Página anterior" não deve ser exibida e, se o usuário já estiver na última página, a opção "Próxima página" não deve ser exibida.
+
+Para entender como a paginação funciona, leia a [documentação da API](https://www.anapioficeandfire.com/Documentation#pagination).
+
+### 7 - Quando o usuário selecionar um livro, exibir na tela o JSON daquele livro
+
+**Atenção:** As propriedades `characters` e `povCharacters` não deverão ser exibidas.
+
+### 8 - Sempre exibir uma opção de voltar
+
+Em todos os menus, uma opção de "voltar" deve ser exibida. Essa opção deve levar o usuário para o menu anterior e, através dela, deve ser possível chegar de volta até o menu principal
+
+## Bônus
+
+### 9 - Exibir opção de listar as casas do mundo de Game of Thrones
+
+Exibir, no menu principal, um menu "casas" e, dentro dele, uma opção "Listar casas".
+
+O comportamento deve ser idêntico ao de listar personagens, inclusive a paginação, que deve atender ao [requisito 6](#6---apresentar-as-opções-próxima-página-e-página-anterior-caso-existam-mais-de-10-resultados).
+
+**Atenção** A propriedade `swornMembers` não deve ser exibida ao selecionar uma casa.
+
+## Instruções para entregar seu projeto:
+
+### ANTES DE COMEÇAR A DESENVOLVER:
+
+1. Clone o repositório
+  * `git clone git@github.com:tryber/sd-01-block27-a-cli-of-ice-and-fire.git`.
+  * Entre na pasta do repositório que você acabou de clonar:
+    * `sd-01-block27-a-cli-of-ice-and-fire`
+
+2. Instale as dependências
+  * `npm install`
+
+3. Crie uma branch a partir da branch `master`
+  * Verifique que você está na branch `master`
+    * Exemplo: `git branch`
+  * Se não estiver, mude para a branch `master`
+    * Exemplo: `git checkout master`
+  * Agora crie uma branch para qual você vai submeter os `commits` do seu projeto
+    * Você deve criar uma branch no seguinte formato: `nome-de-usuario-nome-do-projeto`
+    * Exemplo: `git checkout -b joaozinho-a-cli-of-ice-and-fire`
+
+4. Para cada menu novo que você for adicionar, crie um diretório dentro de `lib/menus`, e adicione os arquivos do menu lá. Por exemplo, crie um menu `books` e adicione-o ao index da pasta `menus`:
+
+```sh
+mkdir lib/menus/books
+touch lib/menus/books/index.js
+```
+
+```javascript
+module.exports = function() {
+    console.log('Menu de livros')
+}
+```
+
+```javascript
+// lib/menus/index.js
+const books = require('./books');
+const characters = require('./characters');
+
+module.exports = {
+  books
+  characters
+};
+
+```
+
+5. Adicione as mudanças ao _stage_ do Git e faça um `commit`
+  * Verifique que as mudanças ainda não estão no _stage_
+    * Exemplo: `git status` (deve aparecer listado o arquivo _src/last.js_ em vermelho)
+  * Adicione o arquivo alterado ao _stage_ do Git
+      * Exemplo:
+        * `git add .` (adicionando todas as mudanças - _que estavam em vermelho_ - ao stage do Git)
+        * `git status` (deve aparecer listado o arquivo _src/last.js_ em verde)
+  * Faça o `commit` inicial
+      * Exemplo:
+        * `git commit -m 'iniciando o projeto. VAMOS COM TUDO :rocket:'` (fazendo o primeiro commit)
+        * `git status` (deve aparecer uma mensagem tipo _nothing to commit_ )
+
+6. Adicione a sua branch com o novo `commit` ao repositório remoto
+  * Usando o exemplo anterior: `git push -u origin joaozinho-a-cli-of-ice-and-fire`
+
+7. Crie um novo `Pull Request` _(PR)_
+  * Vá até a página de _Pull Requests_ do [repositório no GitHub](https://github.com/tryber/sd-01-block27-a-cli-of-ice-and-fire/pulls)
+  * Clique no botão verde _"New pull request"_
+  * Clique na caixa de seleção _"Compare"_ e escolha a sua branch **com atenção**
+  * Clique no botão verde _"Create pull request"_
+  * Adicione uma descrição para o _Pull Request_ e clique no botão verde _"Create pull request"_
+  * **Não se preocupe em preencher mais nada por enquanto!**
+  * Volte até a [página de _Pull Requests_ do repositório](https://github.com/tryber/sd-01-block27-a-cli-of-ice-and-fire/pulls) e confira que o seu _Pull Request_ está criado
+
+---
+
+### DURANTE O DESENVOLVIMENTO
+
+* ⚠ **LEMBRE-SE DE CRIAR TODOS OS ARQUIVOS DENTRO DA PASTA COM O SEU NOME** ⚠
+
+* Faça `commits` das alterações que você fizer no código regularmente
+
+* Lembre-se de sempre após um (ou alguns) `commits` atualizar o repositório remoto
+
+* Os comandos que você utilizará com mais frequência são:
+  1. `git status` _(para verificar o que está em vermelho - fora do stage - e o que está em verde - no stage)_
+  2. `git add` _(para adicionar arquivos ao stage do Git)_
+  3. `git commit` _(para criar um commit com os arquivos que estão no stage do Git)_
+  4. `git push -u nome-da-branch` _(para enviar o commit para o repositório remoto na primeira vez que fizer o `push` de uma nova branch)_
+  5. `git push` _(para enviar o commit para o repositório remoto após o passo anterior)_
+
+---
+
+### DEPOIS DE TERMINAR O DESENVOLVIMENTO
+
+Para **"entregar"** seu projeto, siga os passos a seguir:
+
+* Vá até a página **DO SEU** _Pull Request_, adicione a label de _"code-review"_ e marque seus colegas
+  * No menu à direita, clique no _link_ **"Labels"** e escolha a _label_ **code-review**
+  * No menu à direita, clique no _link_ **"Assignees"** e escolha **o seu usuário**
+  * No menu à direita, clique no _link_ **"Reviewers"** e digite `students`, selecione o time `tryber/students-sd-02`
+
+Se ainda houver alguma dúvida sobre como entregar seu projeto, [aqui tem um video explicativo](https://vimeo.com/362189205).
+
+---
+
+### REVISANDO UM PULL REQUEST
+
+⚠⚠⚠
+
+À medida que você e os outros alunos forem entregando os projetos, vocês serão alertados **via Slack** para também fazer a revisão dos _Pull Requests_ dos seus colegas. Fiquem atentos às mensagens do _"Pull Reminders"_ no _Slack_!
+
+Os monitores também farão a revisão de todos os projetos, e irão avaliar tanto o seu _Pull Request_, quanto as revisões que você fizer nos _Pull Requests_ dos seus colegas!!!
+
+Use o material que você já viu sobre [Code Review](https://course.betrybe.com/real-life-engineer/code-review/) para te ajudar a revisar os projetos que chegaram para você.
---
+++ test/fixtures/characters.js
@@ -0,0 +1,775 @@
+
+const firstPageBody = [
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/1',
+    name: '',
+    gender: 'Female',
+    culture: 'Braavosi',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'The Daughter of the Dusk',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/2',
+    name: 'Walder',
+    gender: 'Male',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'Hodor',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [
+      'https://www.anapioficeandfire.com/api/houses/362',
+    ],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/1',
+      'https://www.anapioficeandfire.com/api/books/2',
+      'https://www.anapioficeandfire.com/api/books/3',
+      'https://www.anapioficeandfire.com/api/books/5',
+      'https://www.anapioficeandfire.com/api/books/8',
+    ],
+    povBooks: [],
+    tvSeries: [
+      'Season 1',
+      'Season 2',
+      'Season 3',
+      'Season 4',
+      'Season 6',
+    ],
+    playedBy: [
+      'Kristian Nairn',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/3',
+    name: '',
+    gender: 'Male',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'Lamprey',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [
+      'https://www.anapioficeandfire.com/api/houses/15',
+    ],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/3',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/4',
+    name: '',
+    gender: 'Female',
+    culture: 'Braavosi',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'The Merling Queen',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+      'https://www.anapioficeandfire.com/api/books/8',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/5',
+    name: '',
+    gender: 'Male',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'Old Crackbones',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/6',
+    name: '',
+    gender: 'Female',
+    culture: 'Braavosi',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'The Poetess',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/7',
+    name: '',
+    gender: 'Female',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'Porridge',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [
+      'https://www.anapioficeandfire.com/api/houses/15',
+    ],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/3',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/8',
+    name: '',
+    gender: 'Male',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'Quickfinger',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [
+      'https://www.anapioficeandfire.com/api/houses/23',
+    ],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/6',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/9',
+    name: '',
+    gender: 'Female',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'the Sailor\'s Wife',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/10',
+    name: '',
+    gender: 'Female',
+    culture: 'Braavosi',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'The Veiled Lady',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+];
+
+const secondPageBody = [
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/11',
+    name: '',
+    gender: 'Female',
+    culture: 'Braavosi',
+    born: 'In 263 AC or 264 AC',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'The waif',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+      'https://www.anapioficeandfire.com/api/books/8',
+    ],
+    povBooks: [],
+    tvSeries: [
+      'Season 5',
+      'Season 6',
+    ],
+    playedBy: [
+      'Faye Marsay',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/12',
+    name: 'Balon Greyjoy',
+    gender: 'Male',
+    culture: '',
+    born: '',
+    died: '299 AC, at Pyke',
+    titles: [
+      'Iron King',
+      'King of the Isles and the North',
+      'King of Salt and Rock',
+      'Son of the Sea Wind',
+      'Lord Reaper of Pyke',
+      'The Greyjoy',
+      'Captain of the Great Kraken',
+    ],
+    aliases: [
+      'Balon the Brave',
+      'Balon the Blessed',
+      'Balon the Twice Crowned',
+      'Balon the Widowmaker',
+      'The Kraken King',
+    ],
+    father: 'https://www.anapioficeandfire.com/api/characters/836',
+    mother: '',
+    spouse: 'https://www.anapioficeandfire.com/api/characters/66',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/1',
+      'https://www.anapioficeandfire.com/api/books/2',
+      'https://www.anapioficeandfire.com/api/books/3',
+      'https://www.anapioficeandfire.com/api/books/5',
+      'https://www.anapioficeandfire.com/api/books/8',
+    ],
+    povBooks: [],
+    tvSeries: [
+      'Season 2',
+      'Season 3',
+      'Season 6',
+    ],
+    playedBy: [
+      'Patrick Malahide',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/13',
+    name: 'Chayle',
+    gender: 'Male',
+    culture: '',
+    born: '',
+    died: 'In 299 AC, at Winterfell',
+    titles: [
+      'Septon',
+    ],
+    aliases: [
+      '',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/1',
+      'https://www.anapioficeandfire.com/api/books/2',
+      'https://www.anapioficeandfire.com/api/books/3',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/14',
+    name: 'Gillam',
+    gender: 'Male',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      'Brother',
+    ],
+    aliases: [
+      '',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/15',
+    name: 'High Septon',
+    gender: 'Male',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      'High Septon',
+      'His High Holiness',
+      'Father of the Faithful',
+      'Voice of the Seven on Earth',
+    ],
+    aliases: [
+      'The High Sparrow',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+      'https://www.anapioficeandfire.com/api/books/8',
+    ],
+    povBooks: [],
+    tvSeries: [
+      'Season 5',
+      'Season 6',
+    ],
+    playedBy: [
+      'Jonathan Pryce',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/16',
+    name: 'Margaery Tyrell',
+    gender: 'Female',
+    culture: 'Westeros',
+    born: 'In 283 AC, at Highgarden',
+    died: '',
+    titles: [
+      'Queen of the Seven Kingdoms',
+    ],
+    aliases: [
+      'The Little Queen',
+      'The Little Rose',
+      'Maid Margaery',
+    ],
+    father: '',
+    mother: '',
+    spouse: 'https://www.anapioficeandfire.com/api/characters/862',
+    allegiances: [
+      'https://www.anapioficeandfire.com/api/houses/398',
+    ],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/1',
+      'https://www.anapioficeandfire.com/api/books/2',
+      'https://www.anapioficeandfire.com/api/books/3',
+      'https://www.anapioficeandfire.com/api/books/5',
+      'https://www.anapioficeandfire.com/api/books/8',
+    ],
+    povBooks: [],
+    tvSeries: [
+      'Season 2',
+      'Season 3',
+      'Season 4',
+      'Season 5',
+      'Season 6',
+    ],
+    playedBy: [
+      'Natalie Dormer',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/17',
+    name: 'Melicent',
+    gender: 'Female',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      'Septa',
+    ],
+    aliases: [
+      '',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/18',
+    name: 'Meribald',
+    gender: 'Male',
+    culture: '',
+    born: 'In 247 AC or 248 AC, at the riverlands',
+    died: '',
+    titles: [
+      'Septon',
+    ],
+    aliases: [
+      '',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/19',
+    name: 'Moelle',
+    gender: 'Female',
+    culture: '',
+    born: '',
+    died: '',
+    titles: [
+      'Septa',
+    ],
+    aliases: [
+      '',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+      'https://www.anapioficeandfire.com/api/books/8',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/20',
+    name: 'Mordane',
+    gender: 'Female',
+    culture: '',
+    born: '',
+    died: 'In 298 AC, at King\'s Landing',
+    titles: [
+      'Septa',
+    ],
+    aliases: [
+      '',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [
+      'https://www.anapioficeandfire.com/api/houses/362',
+    ],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/1',
+      'https://www.anapioficeandfire.com/api/books/2',
+      'https://www.anapioficeandfire.com/api/books/3',
+    ],
+    povBooks: [],
+    tvSeries: [
+      'Season 1',
+    ],
+    playedBy: [
+      'Susan Brown',
+    ],
+  },
+];
+
+const lastPageBody = [
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/2135',
+    name: 'Zekko',
+    gender: 'Male',
+    culture: 'Dothraki',
+    born: '',
+    died: '',
+    titles: [
+      'Khal',
+    ],
+    aliases: [
+      '',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/8',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/2136',
+    name: 'Zharaq zo Loraq',
+    gender: 'Male',
+    culture: 'Ghiscari',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'The Liberator',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/8',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/2137',
+    name: 'Zollo',
+    gender: 'Male',
+    culture: 'Dothraki',
+    born: '',
+    died: '',
+    titles: [
+      '',
+    ],
+    aliases: [
+      'Zollo the Fat',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/3',
+      'https://www.anapioficeandfire.com/api/books/5',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+  {
+    url: 'https://www.anapioficeandfire.com/api/characters/2138',
+    name: 'Criston Cole',
+    gender: 'Male',
+    culture: 'Stormlands',
+    born: 'In 82 AC, at Blackhaven',
+    died: 'In 130 AC, at the Butcher\'s Ball south of the Gods Eye',
+    titles: [
+      'Ser',
+      'Lord Commander of the Kingsguard',
+      'Hand of the King',
+    ],
+    aliases: [
+      'Criston the Kingmaker',
+    ],
+    father: '',
+    mother: '',
+    spouse: '',
+    allegiances: [
+      'https://www.anapioficeandfire.com/api/houses/77',
+    ],
+    books: [
+      'https://www.anapioficeandfire.com/api/books/5',
+      'https://www.anapioficeandfire.com/api/books/9',
+      'https://www.anapioficeandfire.com/api/books/10',
+      'https://www.anapioficeandfire.com/api/books/11',
+    ],
+    povBooks: [],
+    tvSeries: [
+      '',
+    ],
+    playedBy: [
+      '',
+    ],
+  },
+];
+
+const linkHeaders = {
+  hasNext: '<https://www.anapioficeandfire.com/api/characters?page=2&pageSize=10>; rel="next", <https://www.anapioficeandfire.com/api/characters?page=1&pageSize=10>; rel="first", <https://www.anapioficeandfire.com/api/characters?page=214&pageSize=10>; rel="last"',
+  hasPrevious: '<https://www.anapioficeandfire.com/api/characters?page=213&pageSize=10>; rel="prev", <https://www.anapioficeandfire.com/api/characters?page=1&pageSize=10>; rel="first", <https://www.anapioficeandfire.com/api/characters?page=214&pageSize=10>; rel="last"',
+  hasBoth: '<https://www.anapioficeandfire.com/api/characters?page=3&pageSize=10>; rel="next", <https://www.anapioficeandfire.com/api/characters?page=1&pageSize=10>; rel="prev", <https://www.anapioficeandfire.com/api/characters?page=1&pageSize=10>; rel="first", <https://www.anapioficeandfire.com/api/characters?page=214&pageSize=10>; rel="last"',
+};
+
+const responses = {
+  hasNext: { body: firstPageBody, headers: { link: linkHeaders.hasNext } },
+  hasPrevious: { body: lastPageBody, headers: { link: linkHeaders.hasPrevious } },
+  hasBoth: { body: secondPageBody, headers: { link: linkHeaders.hasBoth } },
+};
+
+function getNames(body) {
+  return body.map(({ name, aliases }) => name || aliases[0]);
+}
+
+const names = {
+  hasNext: getNames(firstPageBody),
+  hasPrevious: getNames(lastPageBody),
+  hasBoth: getNames(secondPageBody),
+};
+
+module.exports = {
+  responses,
+  names,
+};
